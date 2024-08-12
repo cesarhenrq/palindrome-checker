8f76082f92d0e299fcf3665ad345465aa8528de0
Cééesar Henrique <cesaarhvb@gmail.com>
2024-08-12 18:57:57 -0300

    feat: implement palindrome checker functionality in script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+const checkPalindrome = () => {
+  if (textInput.value === "") {
+    alert("Please enter a text");
+    return;
+  }
+
+  const text = textInput.value.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversedText = text.split("").reverse().join("");
+
+  result.innerHTML =
+    text === reversedText
+      ? `
+    <span id="result-text">${textInput.value}</span> is a palindrome
+    `
+      : `
+    <span id="result-text">${textInput.value}</span> is not a palindrome`;
+};
+
+checkButton.addEventListener("click", checkPalindrome);
